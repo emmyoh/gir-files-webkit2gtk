9eb73601bad4a98cd7163d983b1b77c04719a51a
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2022-01-29 18:24:50 -0500

    Move out of subdirectory

--- dl.py
+++ dl.py
@@ -3,5 +3,5 @@ import os
 from distutils.dir_util import copy_tree
 
 src_dir = "/usr/share/gir-1.0/"
-dest_dir = os.path.abspath("./gir_files/")
+dest_dir = os.path.abspath("./")
 copy_tree(src_dir, dest_dir)
