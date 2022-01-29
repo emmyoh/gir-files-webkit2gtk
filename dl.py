ae754c6b7b21e278a99bf47fc3731f650ad625a3
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2022-01-29 00:44:53 -0500

    Initial commit

---
+++ dl.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import os
+import shutil
+
+GIR_FILES=[
+"JavaScriptCore-5.0", "Soup-3.0", "WebKit2-5.0", "WebKit2WebExtension-5.0",
+]
+
+dest_dir = os.path.abspath("./")
+
+for file in GIR_FILES:
+    src = f"/usr/share/gir-1.0/{file}.gir"
+    dest = f"{dest_dir}/{file}.gir"
+    try:
+        shutil.copy(src, dest)
+    except FileNotFoundError:
+        print(f"gir file not found {file}")
