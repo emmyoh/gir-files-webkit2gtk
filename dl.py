f6d987e63fe8c57206a749eabd2dd30af233fd11
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2022-01-29 17:42:01 -0500

    Copy all GIR files

--- dl.py
+++ dl.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python3
 import os
-import shutil
+from distutils.dir_util import copy_tree
 
-GIR_FILES=[
-"JavaScriptCore-5.0", "Soup-3.0", "WebKit2-5.0",
-"WebKit2WebExtension-5.0", "freetype2-2.0", "GLib-2.0",
-]
-
+src_dir = "/usr/share/gir-1.0/"
 dest_dir = os.path.abspath("./")
-
-for file in GIR_FILES:
-    src = f"/usr/share/gir-1.0/{file}.gir"
-    dest = f"{dest_dir}/{file}.gir"
-    try:
-        shutil.copy(src, dest)
-    except FileNotFoundError:
-        print(f"gir file not found {file}")
+copy_tree(src_dir, dest_dir)
