f1be7a68a177e94af184fadb17054637b9219269
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2022-01-29 17:45:38 -0500

    Update path to files

--- dl.py
+++ dl.py
@@ -3,5 +3,5 @@ import os
 from distutils.dir_util import copy_tree
 
 src_dir = "/usr/share/gir-1.0/"
-dest_dir = os.path.abspath("./")
+dest_dir = os.path.abspath("./gir_files/")
 copy_tree(src_dir, dest_dir)
